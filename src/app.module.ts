93fbc87ba6a733ab7a2123810f8194c0ae47bf07
saychool <mbekqwerty@gmail.com>
2025-09-22 11:23:48 +0700

    feat: fix fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './modules/user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [PrismaModule, UserModule],
   controllers: [],
   providers: [],
 })
